52f120e9b7e114bc64c239296ac00d99f397bca5
Zibobobo <93097798+Zibobobo@users.noreply.github.com>
2021-12-10 07:52:49 -0500

    Update space_survival.py

--- space survival/space_survival.py
+++ space survival/space_survival.py
@@ -446,12 +446,9 @@ while running:
     
     clock.tick(FPS)
     # user input
-    #player.cooldown()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        #elif event.type == pygame.KEYDOWN:
-            #if event.key == pygame.K_SPACE:
     player.shoot()
 
     # update game
